ee8dc2105cc447dd3f010d31a19619ebcaa222ea
thebigsleepjoe <github@bigjoe.dev>
2024-12-05 21:09:30 -0800

    feat: add more stone footstep sounds and remove a couple of annoying ones

--- src/main/java/ambiencemod/ambience/sounds/footsteps/FootstepsStone.java
+++ src/main/java/ambiencemod/ambience/sounds/footsteps/FootstepsStone.java
@@ -4,10 +4,15 @@ import ambiencemod.ambience.sounds.FootstepsAmbient;
 
 public class FootstepsStone extends FootstepsAmbient {
     public FootstepsStone() {
-        this.addSoundPath("footsteps/Stone4.ogg");
-        this.addSoundPath("footsteps/Stone6.ogg");
-        this.addSoundPath("footsteps/Stone7.ogg");
-        this.addSoundPath("footsteps/Stone8.ogg");
-        this.addSoundPath("footsteps/Stone9.ogg");
+        this.addSoundPath("footsteps/Stone-01.ogg");
+        this.addSoundPath("footsteps/Stone-02.ogg");
+        this.addSoundPath("footsteps/Stone-03.ogg");
+        this.addSoundPath("footsteps/Stone-04.ogg");
+        this.addSoundPath("footsteps/Stone-05.ogg");
+        this.addSoundPath("footsteps/Stone-06.ogg");
+        this.addSoundPath("footsteps/Stone-07.ogg");
+        this.addSoundPath("footsteps/Stone-08.ogg");
+        this.addSoundPath("footsteps/Stone-09.ogg");
+        this.addSoundPath("footsteps/Stone-10.ogg");
     }
 }
