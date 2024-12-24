0fa12e7409326892bd2adf43877e24343d13ac22
thebigsleepjoe <github@bigjoe.dev>
2024-12-24 02:18:14 -0800

    fix(console): stop console spam when player walks on tiles by commenting out relevant snippet

--- src/main/java/ambiencemod/ambience/sounds/footsteps/FootstepsManager.java
+++ src/main/java/ambiencemod/ambience/sounds/footsteps/FootstepsManager.java
@@ -124,9 +124,9 @@ public class FootstepsManager {
         FootstepsAmbient match = this.tileHashMap.get(tclass);
         Boolean isPlayerTile = this.playerTiles.getOrDefault(tclass, false);
 
-        if (mob.isPlayer) {
-            System.out.println("Player tile name: " + tile.getDisplayName() + " | " + tile.getClass().getName());
-        }
+//        if (mob.isPlayer) {
+//            System.out.println("Player tile name: " + tile.getDisplayName() + " | " + tile.getClass().getName());
+//        }
 
         if (match == null) {
             if (isPlayerTile != true) return;
